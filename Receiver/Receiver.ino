c27967bf7494133ac08a7a29c5cb6b197e691800
Ahasan <ahasanbhuiyan06@gmail.com>
2022-07-02 13:03:21 -0700

    Added NRF24, removed SD card

---
+++ Receiver/Receiver.ino
@@ -0,0 +1,45 @@
+#include <SPI.h>
+#include <RH_NRF24.h>
+
+RH_NRF24 nrf24(8, 7);
+
+struct sensorData{
+  float temp;
+  float relHumid;
+  float dewPoint;
+  float pressure;
+}; sensorData dataRX;
+
+void setup() {
+  Serial.begin(9600);
+  while(!Serial){
+  }
+  
+  if(!nrf24.init()){
+    Serial.println("init failed");
+  }
+  if(!nrf24.setChannel(1)){
+    Serial.println("setChannel failed");
+  }
+  if(!nrf24.setRF(RH_NRF24::DataRate1Mbps, RH_NRF24::TransmitPower0dBm)){
+    Serial.println("setRF failed");
+  }
+}
+
+void loop() {
+  if(nrf24.available()){
+    uint8_t len = sizeof(dataRX);
+    if(nrf24.recv((uint8_t *)&dataRX, &len)){
+      Serial.print(dataRX.temp);
+      Serial.print(", ");
+      Serial.print(dataRX.relHumid);
+      Serial.print(", ");
+      Serial.print(dataRX.dewPoint);
+      Serial.print(", ");
+      Serial.println(dataRX.pressure);
+    }
+    else{
+      Serial.println("RX failed");
+    }
+  }
+}
---
+++ Transmitter/Transmitter.ino
@@ -0,0 +1,81 @@
+#include <DHT.h>
+#include <SPI.h>
+#include <RH_NRF24.h>
+#include <Adafruit_BMP280.h>
+
+const uint8_t dhtPin = 2;
+const uint8_t dhtType = DHT22;
+
+DHT dht(dhtPin, dhtType);
+RH_NRF24 nrf24(8, 7);
+Adafruit_BMP280 bmp;
+
+struct sensorData{
+  float temp;
+  float relHumid;
+  float dewPoint;
+  float pressure;
+}; sensorData dataTX;
+
+float getDewPoint(float tempData, float relHumidData){
+  // Equation from https://www.omnicalculator.com/physics/dew-point
+  
+  // Magnus coefficients
+  const float a = 17.625;
+  const float b = 243.04;
+
+  // Calculate intermediary value "alpha" which is used to calculate dew point
+  float alpha = log(relHumidData / 100) + (a * tempData) / (b + tempData);
+  float dewPoint = (b * alpha) / (a - alpha);
+
+  return dewPoint;
+}
+
+void setup() {
+  Serial.begin(9600);
+  while(!Serial){
+  }
+  
+  dht.begin();
+
+  if(!nrf24.init()){
+    Serial.println("NRF24 init. failed");
+  }
+  if(!nrf24.setChannel(1)){
+    Serial.println("setChannel failed");
+  }
+  if(!nrf24.setRF(RH_NRF24::DataRate1Mbps, RH_NRF24::TransmitPower0dBm)){
+    Serial.println("setRF failed");
+  }
+
+  if(!bmp.begin()){
+    Serial.println("BMP280 initialization failed");
+    // Don't do anything more
+    while(1);
+  }
+
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+}
+
+void loop() {
+  
+  dataTX.temp = dht.readTemperature();            
+  dataTX.relHumid = dht.readHumidity();           
+  dataTX.dewPoint = getDewPoint(dataTX.temp, dataTX.relHumid);  
+  dataTX.pressure = bmp.readPressure();
+
+  //Serial.print("Temp: ");           Serial.print(dataTX.temp);
+  //Serial.print(" C, Rel Humid: ");  Serial.print(dataTX.relHumid);
+  //Serial.print(" %, Dew Point: ");  Serial.print(dataTX.dewPoint);
+  //Serial.print(" C, Pressure: ");   Serial.print(dataTX.pressure); Serial.println(" kPa");
+
+  nrf24.send((uint8_t*)&dataTX, sizeof(dataTX));
+  nrf24.waitPacketSent();
+
+  // DHT22 can only update readings every 2sec, so wait at least 2sec before running again
+  delay(2000);
+}
